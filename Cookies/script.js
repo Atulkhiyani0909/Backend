46afa3d2cc789eac3f4a6a3237a1a6d794ee042b
Atul <atulkhiyani09@gmail.com>
2024-10-10 22:55:01 +0530

    updated signed cookie part

--- Cookies/script.js
+++ Cookies/script.js
@@ -1,59 +1,41 @@
-const express=require('express');
+const express = require('express');
+const cookieParser = require('cookie-parser'); // to read the values of the cookies
 
-const cookieParser=require('cookie-parser');// to read the values of the cookies
+const app = express();
 
-const app=express();
-//cookies are very use full they help to store the little amount of data to be stored on the user system 
-//cookies help in authntication and authorization
-
-//to use cookie parser
-app.use(cookieParser());
-
-
- app.use(cookieParser({ secret: 'your-secret-key' }));// this is the secrete key for our cookie signed cookie doesn't store the cookie value directly but save like the use of the secret key in encrypted format
-
-
+// Use cookie parser with a secret key for signed cookies
+app.use(cookieParser( "HiddenCode")); // Ensure this is a valid non-empty string  for making our cookies signed and save 
 
 // In this we will learn how to send cookies to the user 
-
-app.get("/getcookies",(req,res)=>{
-    res.cookie("MadeIn","India");
-    res.cookie("great","hello-user");
-    res.send(' send you some cookies');
-});
-
-
-app.get("/greet",(req,res)=>{
-    let {name="anonymous"}=req.cookies;//this is will read if the name value is there in the cookie in system than run Hi name .
-    res.send(`Hi , ${name}`);
-})
-
-
-// ... existing code ...
-
-app.use(cookieParser({ secret: 'your-secret-key' })); // Ensure cookie parser is set up for signed cookies
-
-
-//this is the signed cookie 
 app.get("/getcookies", (req, res) => {
-    res.cookie("MadeIn", "India", { signed: true }); // Set a signed cookie
-    res.cookie("great", "hello-user", { signed: true }); // Set another signed cookie
-    res.send('Sent you some signed cookies');
+    res.cookie("MadeIn", "India"); // Set a signed cookie
+    res.cookie("great", "hello-user"); // Set another signed cookie
+    res.send('Sent you some cookies');
 });
 
 app.get("/greet", (req, res) => {
-    let { name = "anonymous" } = req.signedCookies; // Read signed cookies
-    res.send(`Hi, ${name}`);
+    // Ensure you are reading the correct cookie name
+    let { great = "anonymous" } = req.cookies; // Read the 'great' cookie
+    res.send(`Hi, ${great}`); // Respond with the value of the 'great' cookie
 });
 
-
-
-app.get("/",(req,res)=>{
-    console.dir(req.cookies);//to read the values of the cookies
-    res.send("this is the root path");
+// This is the signed cookie 
+app.get("/get", (req, res) => {
+    res.cookie("name", "I", { signed: true }); // Set a signed cookie
+    
+    res.send('Sent you some signed cookies');
 });
 
+app.get("/greetsignedcookie", (req, res) => {
+    let { great = "anonymous" } = req.signedCookies; // Read the 'great' signed cookie
+    res.send(`Hi, ${great}`); // Respond with the value of the 'great' cookie
+});
 
-app.listen(3000,()=>{
-    console.log("app listining on the port 3000");
-})
+app.get("/", (req, res) => {
+    console.dir(req.cookies); // To read the values of the cookies
+    res.send("This is the root path");
+});
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000");
+});
